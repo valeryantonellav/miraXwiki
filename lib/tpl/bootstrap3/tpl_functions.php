b6d966f4484caf02a390f454dd0792b62b76334a
Valery Vallejo <valery.antonella19@gmail.com>
2026-05-21 09:37:03 -0500

    chore: remove unused themes

--- lib/tpl/bootstrap3/tpl_functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Fallback script for compatibility with Bootstrap Wrapper and Icons plugins
-
-dbg_deprecated('Upgrade Bootstrap Wrapper and Icons plugins');
-
-include_once 'tpl/functions.php';
